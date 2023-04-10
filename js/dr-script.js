d5d951e2b7e889173b75bb18f56a3d894daee8fd
ptrk <patrick.m.marschke@gmail.com>
2023-04-09 21:54:59 -0500

    finished audioplayer component and landing index

--- js/dr-script.js
+++ js/dr-script.js
@@ -1,6 +1,10 @@
 import initWiki from "./initWiki.js";
+import { ESAudioPlayer } from "./components/ESAudioPlayer.js";
+import { ESVideoPlayer } from "./components/ESVideoPlayer.js";
 
 if (document.querySelectorAll("[data-wiki]").length > 0) {
   initWiki();
   console.log(document.querySelectorAll("[data-wiki]").length);
 }
+customElements.define("es-audio", ESAudioPlayer);
+customElements.define("es-video", ESVideoPlayer);
